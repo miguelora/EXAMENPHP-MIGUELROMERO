72db1aa6b1edf39be9637fdc81bf246bfab5d759
Miguel <miguel_romerillo@hotmail.com>
2021-11-29 09:24:43 +0100

    29 octubre "No terminado"

---
+++ EXAMEN2 PHP/Ejercicio3.php
@@ -0,0 +1,49 @@
+<?php
+
+    $pali = $_POST["pali"];
+    $operador = $_POST["enviar"];
+
+    if (condition) {
+        # code...
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
